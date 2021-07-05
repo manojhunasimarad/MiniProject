687ed32da13928e4957955939d5cee574cc9f7da
Shaik Mohammed Feroz <mohammedferoz17@gmail.com>
2021-07-05 15:22:02 +0530

    code started

--- 3_Impelmentation/main.c
+++ 3_Impelmentation/main.c
@@ -1,6 +1,25 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-07-05
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include<stdio.h>
+#include<string.h>
+#include<hms.h>
 
 int main()
-    {
-        code();
-    }
+{
+    printf("\t\t\t\t\t\t Hospital Management System\n");
+    printf("\t\t\t\t\t\t*********************************\n");
+    printf("\n\n\n\n");
+     void menu();
+    
+
+
+
+}
